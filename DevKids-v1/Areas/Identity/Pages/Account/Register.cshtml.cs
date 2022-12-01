36cdf313d7cb3d38eb797f84070ef4af64ad3c19
Fabio Barbieri <fabio.barbieri777@outlook.com>
2022-12-01 10:34:41 -0300

    html Register

--- DevKids-v1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DevKids-v1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -158,8 +158,8 @@ namespace DevKids_v1.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirme seu email",
+                        $"Por favor, confirme a sua conta na Dev Kids Academy <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicando aqui</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
